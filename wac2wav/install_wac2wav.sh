c8ac55851fae2925ba38c6333dadd0151652bd76
Luis J. Villanueva <ljvillanueva@coquipr.com>
2014-07-09 11:27:01 -0400

    Adding a script to install the wac2wav converter

---
+++ wac2wav/install_wac2wav.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# 9 Jul 2014
+#Script that installs the wac2wav converter from 
+# Wildlife Acoustics.
+# 
+
+# Set the path where Pumilio will be installed inside /var/www/
+# If "/var/www/pumilio", then set pumiliodir="pumilio" without trailing slash
+
+pumiliodir="pumilio"
+
+# Then, run as sudo:
+#	sudo ./install_wac2wav.sh
+
+#=================================================
+# Script body
+#=================================================
+
+#check if can user is sudoer
+if [ "$(whoami)" != "root" ]; then
+	echo "Please run as root or using sudo."
+	exit 1
+fi
+
+#check if gcc is installed
+if ! type "__gcc" > /dev/null; then
+	echo ""
+	echo "gcc not found. Please install compiler using:"
+	echo " sudo apt-get install build-essential"
+	echo ""
+	exit 1
+fi
+
+#Random dir
+tmpdir=$RANDOM
+
+mkdir /tmp/$tmpdir
+cd /tmp/$tmpdir
+
+echo " Downloading source..."
+echo " "
+
+#Get source
+wget http://www.wildlifeacoustics.com/downloads/wac2wavcmd-1.0.zip
+unzip wac2wavcmd-1.0.zip
+cd wac2wavcmd-1.0
+
+echo " Compiling..."
+echo " "
+
+#compile
+make
+
+echo " Installing to /usr/local/bin/"
+echo " "
+
+cp wac2wavcmd /usr/local/bin/
+
